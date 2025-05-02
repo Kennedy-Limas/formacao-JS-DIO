a195d5af5f3dfd6b93c8c01b3c2644965c014bc9
Kennedy <ka.dl@hotmail.com>
2025-05-02 14:35:17 -0300

    first commit

---
+++ aulas/aula1.js
@@ -0,0 +1,17 @@
+//Desafio 
+
+//Faça um programa para calcular o valor de uma viagem.
+//Você terá 3 variáveis. Sendo elas:
+//     1 - Preço do combustivel
+//     2 - Gasto médio de combustível do carro por KM;
+//     3 - Distância em KM da veiagem;
+
+// Imprima no console o valor que será gasto para realizar esta viagem.
+
+let precoCombustivel = 5.98;
+let distanciaKM = 350;
+
+
+let gastoMedio = distanciaKM / precoCombustivel;
+
+console.log(gastoMedio);
