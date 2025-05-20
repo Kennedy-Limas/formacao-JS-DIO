8e6c8676a77bdcd095003354da464ba3e9a81881
Kennedy <ka.dl@hotmail.com>
2025-05-20 10:33:45 -0300

    estudos sobre objetos e classes

--- objetos/aula1.js
+++ objetos/aula1.js
@@ -1,10 +1,44 @@
-const Kennedy = {
-    nome: 'Kennedy A. Lima',
-    idade: 28
+//Instancia é uma ocorrencia 
+// const pessoa = {
+//     nome: 'Kennedy',
+//     idade: 28,
+
+//     descrever: function(){
+//         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+//     }
+
+// }
+
+// pessoa.descrever();
+
+//Classe é uma definição
+class Pessoa {
+    nome;
+    idade;
+    anoDeNascimento;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2025 - idade
+    }
+
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
 }
 
-Kennedy.altura = 1.83;
+function compararPessoas(p1, p2) {
+    if (p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+    } else if (p2.idade > p1.idade) {
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
+    }
+}
 
-delete Kennedy.nome; 
+const teste1 = new Pessoa('Kennedy', 23);
+const teste2 = new Pessoa('João', 23);
 
-console.log(Kennedy)
+compararPessoas(teste1, teste2);
