d9f9b25e14c28102786d9e7b1c020f55c326d375
Kennedy <ka.dl@hotmail.com>
2025-05-23 12:43:41 -0300

    Importação e destruct de objetos importados

--- 04array/listaExercicios.js
+++ 04array/listaExercicios.js
@@ -1,1 +1,32 @@
 // 1) Crie um programa que dado um numero crie a sua tabuada
+
+const tabuada =  5;
+
+for (let i = 0; i <= 10; i++) {
+
+    console.log(i * tabuada)
+    
+}
+
+// 2) Crei um programa que seha capaz de percorrer uma lista de números e imprima cada número par encontrado.
+
+const lista = [0,1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < lista.length; i++) {
+    
+    const numeroDaLista = lista[i]
+
+    if (numeroDaLista  % 2 === 0) {
+
+        // console.log(numeroDaLista)
+
+    }
+
+
+}
+
+ const numeros = [];
+ for (let i = 0; i < 10; i++) {
+   numeros.push(i);
+ }
+ console.log(numeros);
---
+++ 05importacao/main.js
@@ -0,0 +1,10 @@
+// const funcoes = require('./aula1')
+
+// console.log(funcoes)
+// console.log(funcoes.gets())
+
+//utilizando o destruct 
+
+const { gets, print } = require('./aula1')
+
+print(gets())
