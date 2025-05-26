a9390478a4dc8de4d74f6cb472d6cfa20fe07e89
Kennedy <ka.dl@hotmail.com>
2025-05-26 17:00:33 -0300

    Exercicios erefatoração

--- 05importacao/aula1.js
+++ 05importacao/aula1.js
@@ -1,14 +1,20 @@
 
+let entradas = [ 9,2,3,4,5,6,7,8,9,100];
+let i = 0;
+
+
 function gets() {
 
-    return 10;
+    const valor = entradas[i];
+    i++;
+    return valor;
 
 }
 
 
 function print(texto) {
 
-    console.log(texto)
+    console.log(texto);
 
 }
 
--- 05importacao/main.js
+++ 05importacao/main.js
@@ -5,6 +5,61 @@
 
 //utilizando o destruct 
 
+/*
+Uma sala contém  alunos e para cada aluno foi sorteado um numero de 1 a 100.
+Faça um programa que receba os 5 numeros sorteados para os alunos e mostre o maior numero sorteado.
+
+dados de entrada:
+5
+50
+10
+98
+23
+
+saida
+98
+
+*/
+
+
 const { gets, print } = require('./aula1')
 
-print(gets())
+// const numerosSorteados = [];
+
+// for (let i = 0; i < 5; i++) {
+
+//     const numeroSorteados = gets();
+//     numerosSorteados.push(numeroSorteados)
+    
+// }
+
+// let maiorValor = 0
+
+// for (let i = 0; i < numerosSorteados.length; i++) {
+//     const numeroSorteados = numerosSorteados[i];
+//     if(numeroSorteados > maiorValor) {
+//         maiorValor = numeroSorteados
+//     }
+
+// }
+
+// print(maiorValor)
+
+//Refatorando o código acima
+
+//totalmente dinamico não importa o tamnaho da lista
+const quantidadeDeAlunos = gets();
+
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < quantidadeDeAlunos; i++) {
+
+    const numeroSorteados = gets();
+
+    if(numeroSorteados > maiorValorEncontrado){
+        maiorValorEncontrado = numeroSorteados;
+    }
+    
+}
+
+print(maiorValorEncontrado)
