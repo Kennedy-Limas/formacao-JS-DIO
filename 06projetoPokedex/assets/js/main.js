77df21bb4142c9d96f1ca8b7968eb69a4cd83bdf
Kennedy <ka.dl@hotmail.com>
2025-06-12 15:06:55 -0300

    Consumindo a pokeAPI e buscando dados

--- 06projetoPokedex/assets/js/main.js
+++ 06projetoPokedex/assets/js/main.js
@@ -1,10 +1,37 @@
-const offset = 0;
-const limit = 10;
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10';
 
-fetch(url).then(function(response){
-    console.log(response)
-})
 
-const x = 10 + 10
-console.log(x)
+function convertPokemonToList(pokemon) {
+    return`
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+    
+        <div class="detail">
+            <ol class="types" >
+                <li class="type">Grass</li>
+                <li class="type">Poison</li>
+
+            </ol>
+
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+    </li>
+
+    `;
+}
+
+const pokemonListName = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons) => {
+        const listItems = []
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];    
+            listItems.push(convertPokemonToList(pokemon))
+        }
+        console.log(listItems)
+
+    })
+
+
+
